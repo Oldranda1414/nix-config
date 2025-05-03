63b120d9ba10e74c9c49ba15ddf9b580af06da7c
Oldranda1414 <leonardorandacio@gmail.com>
2025-05-03 18:20:05 +0200

    complete setup of git configuration

--- nixos/git.nix
+++ nixos/git.nix
@@ -18,6 +18,53 @@
         alias = "! git config --get-regexp ^alias\\. | sed -e s/^alias\\.// -e s/\\ /\\ =\\ /"; # Pretty list all git aliases
         graph = "log --graph --pretty=format:'%C(magenta)%h %C(white) %an  %ar%C(auto)  %D%n%s%n'"; # Pretty graph log
       };
+      init = {
+        defaultBranch = "main";
+      };
+      pull = {
+        rebase = "true";
+      };
+      log = {
+        abbrevCommit = "true";
+	graphColors = "blue,yellow,cyan,magenta,green,red";
+      };
+      status = {
+        branch = "true";
+	showStash = "true";
+	showUntrackedFiles = "all";
+      };
+      core = {
+        editor = "nvim";
+	pager = "delta";
+      };
+      interactive = {
+        diffFilter = "delta --color-only";
+      };
+      delta = {
+        navigate = "true"; # use n and N to move between diff sections
+	side-by-side = "true";
+
+	# delta detects terminal colors automatically; set one of these to disable auto-detection
+	# dark = true;
+	# light = true;
+      };
+      merge = {
+        conflictstyle = "diff3";
+      };
+      diff = {
+        colorMoved =  "default";
+      };
+      color.decorate = {
+        HEAD = "red";
+	branch = "blue";
+	tag = "yellow";
+	remoteBranch = "magenta";
+      };
     };
   };
+
+  home.packages = with pkgs; [
+    # diff pager
+    delta
+  ];
 }
