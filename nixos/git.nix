4c75ecad6a4309531fbb412c0b49d2a5c9cf4846
Oldranda1414 <leonardorandacio@gmail.com>
2025-05-03 18:00:21 +0200

    refactor: moved alias definition into config from aliases

--- nixos/git.nix
+++ nixos/git.nix
@@ -6,16 +6,18 @@
     userName = "Oldranda1414";
     userEmail = "leonardorandacio@gmail.com";
 
-    aliases = {
-      st = "status -sb"; # Shows concise status
-      last = "log -1 HEAD --stat"; # Shows last commit with file changes
-      cm = "commit -m"; # Shortens commit command
-      gl = "config --global -l"; # Lists global Git configs
-      acm = "!git add . && git commit -am"; # Shortens add all changes and commit
-      lol = "log --oneline"; # Summary of git log
-      pushup = "push -u origin HEAD"; # Sets upstream for new branches
-      alias = "! git config --get-regexp ^alias\\. | sed -e s/^alias\\.// -e s/\\ /\\ =\\ /"; # Pretty list all git aliases
-      graph = "log --graph --pretty=format:'%C(magenta)%h %C(white) %an  %ar%C(auto)  %D%n%s%n'"; # Pretty graph log
+    extraConfig = {
+        alias = {
+          st = "status -sb"; # Shows concise status
+          last = "log -1 HEAD --stat"; # Shows last commit with file changes
+          cm = "commit -m"; # Shortens commit command
+          gl = "config --global -l"; # Lists global Git configs
+          acm = "!git add . && git commit -am"; # Shortens add all changes and commit
+          lol = "log --oneline"; # Summary of git log
+          pushup = "push -u origin HEAD"; # Sets upstream for new branches
+          alias = "! git config --get-regexp ^alias\\. | sed -e s/^alias\\.// -e s/\\ /\\ =\\ /"; # Pretty list all git aliases
+          graph = "log --graph --pretty=format:'%C(magenta)%h %C(white) %an  %ar%C(auto)  %D%n%s%n'"; # Pretty graph log
+      };
     };
   };
 }
