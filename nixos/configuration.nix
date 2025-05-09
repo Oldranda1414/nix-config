1313fc79f3f8ca0a5eb89da80a787120276e4b3d
Oldranda <leonardorandacio@gmail.com>
2025-05-09 11:24:47 +0200

    remove ly settings as they are pretty broken

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -153,12 +153,7 @@
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
 
-  services.displayManager.ly = {
-    enable = true;
-    settings = {
-      asterisk = "\#";
-    };
-  };
+  services.displayManager.ly.enable = true;
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
