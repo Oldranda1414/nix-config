7316dbc4190735386d169a7769fd437d4e4e0307
Oldranda1414 <leonardo.randacio@gmail.com>
2025-05-15 15:21:14 +0200

    make boot partition 3 gb

--- install.sh
+++ install.sh
@@ -20,8 +20,8 @@ umount -R /mnt 2>/dev/null || true
 echo "Partitioning disk $DISK..."
 parted "$DISK" -- mklabel gpt
 
-# Create a 2GB EFI partition from 1MiB to 2049MiB (1MiB + 2048MiB = 2GB)
-parted "$DISK" -- mkpart ESP fat32 1MiB 2049MiB
+# Create a 3GB EFI partition from 1MiB to 3073MiB (1MiB + 3072MiB = 3GB)
+parted "$DISK" -- mkpart ESP fat32 1MiB 3073MiB
 parted "$DISK" -- set 1 esp on  # Set ESP flag on partition 1
 
 # Create the main partition starting right after the EFI partition
