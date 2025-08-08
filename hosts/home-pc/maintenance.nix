cd467aa57c09bc6e128a335225cf2368ba3258ff
Oldranda1414 <leonardorandacio@gmail.com>
2025-08-08 10:39:33 +0200

    fix update service not working, ensure flake inputs follow nixpkgs

--- hosts/home-pc/maintenance.nix
+++ hosts/home-pc/maintenance.nix
@@ -5,16 +5,26 @@
     enable = true;
     flake = inputs.self.outPath;
     flags = [
-      "--update-input"  
+      "--update-input"
       "nixpkgs"
       "-L"
     ];
   };
+
   nix.gc = {
     automatic = true;
     options = "--delete-older-than 5d";
   };
-  nix.optimise = {
-    automatic = true;
+
+  nix.optimise.automatic = true;
+
+  # Override the nixos-upgrade service to wait for network and delay
+  systemd.services.nixos-upgrade = {
+    wants = [ "network-online.target" ];
+    after = [ "network-online.target" ];
+    # serviceConfig.ExecStartPre = [
+    #   # Wait 5 seconds before starting upgrade
+    #   "${pkgs.coreutils}/bin/sleep 5"
+    # ];
   };
 }
